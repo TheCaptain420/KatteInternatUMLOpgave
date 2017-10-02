52dcf38876d584b6c3da2a72babc7ebbe825b18e
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-10-02 12:54:39 +0200

    Har lavet enum i Hund istedet for kat..

--- src/Hund.java
+++ src/Hund.java
@@ -2,7 +2,8 @@ public class Hund {
     int alder=-100;
     String navn = "NONAME";
     String race = "IngenRace";
-
+    int antalBen = 0;
+    String personlighed = "INGEN";
     Hund(String hundensNavn, int hundensAlder, String hundensRace){
         navn = hundensNavn;
         alder =hundensAlder;
@@ -22,5 +23,26 @@ public class Hund {
     public void hvorGammel(){
         System.out.println("Hunden er "+ alder+" gammel.");
     }
+
+    //Tilføj flere get/set
+    public void setPersonlighed(String personlighed){
+        this.personlighed = personlighed;
+    }
+
+    public String getPersonlighed() {
+        return personlighed;
+    }
+
+    public void setAntalBen(int antalBen){
+        this.antalBen = antalBen;
+    }
+
+    public int getAntalBen(){
+        return antalBen;
+    }
+
+    //Har lavet ENUM her, så du kun behøves at "ændre" i en klasse..
+    public enum Farver{n,a,d,e,f,g,s,y,w}
+    Farver hundFarve = Farver.d;
 
 }
